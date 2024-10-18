c7afde1a285df94d0e9edc991ac36a3cf8e664c3
Mike Tickle <mtickle@gmail.com>
2024-10-18 13:35:46 -0400

    Initial

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "192.168.86.58"
+	port     = 5432
+	user     = "pi"
+	password = "Boomer2025"
+	dbname   = "test"
+)
+
+var (
+	street string
+	city   string
+	state  string
+	zip    string
+	lat    string
+	lng    string
+)
+
+type arcgisResults struct {
+	SpatialReference struct {
+		Wkid       int `json:"wkid"`
+		LatestWkid int `json:"latestWkid"`
+	} `json:"spatialReference"`
+	Candidates []struct {
+		Address  string `json:"address"`
+		Location struct {
+			X float64 `json:"x"`
+			Y float64 `json:"y"`
+		} `json:"location"`
+		Score      float64 `json:"score"`
+		Attributes struct {
+		} `json:"attributes"`
+		// Extent struct {
+		// 	Xmin float64 `json:"xmin"`
+		// 	Ymin float64 `json:"ymin"`
+		// 	Xmax float64 `json:"xmax"`
+		// 	Ymax float64 `json:"ymax"`
+		// } `json:"extent"`
+	} `json:"candidates"`
+}
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT street, city, state, zip  FROM address order by address_id desc")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&street, &city, &state, &zip)
+		if err != nil {
+			panic(err)
+		}
+		full_address := street + " " + city + " " + state + " " + zip
+		fmt.Println(full_address)
+
+		//-----------------------------------------------------------------------
+		//--- GEOCODE HERE
+		arcgisUrl := "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?f=pjson&SingleLine=" + (url.QueryEscape(full_address))
+
+		fmt.Println(arcgisUrl)
+		resp, err := http.Get(arcgisUrl)
+		fmt.Println(resp)
+
+		if err != nil {
+			fmt.Println("No response from request")
+		}
+
+		//--- Load all the JSON as text into "body"
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body) // response body is []byte
+
+		//--- Unmarshal the JSON into the "result" value
+		var result arcgisResults
+
+		//fmt.Println(result)
+		if err := json.Unmarshal(body, &result); err != nil { // Parse []byte to the go struct pointer
+
+			fmt.Println("Can not unmarshal JSON")
+		}
+		//-----------------------------------------------------------------------
+
+	}
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+}
