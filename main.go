3b848f729e0001e1fd5fd85c6af0490419d64852
Mike Tickle <mtickle@gmail.com>
2024-10-19 08:45:39 -0400

    Snapshot

--- main.go
+++ main.go
@@ -5,27 +5,33 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"strconv"
+	"strings"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "192.168.86.58"
-	port     = 5432
-	user     = "pi"
-	password = "Boomer2025"
-	dbname   = "test"
-)
+// const (
+// 	host     = "192.168.86.58"
+// 	port     = 5432
+// 	user     = "pi"
+// 	password = "Boomer2025"
+// 	dbname   = "test"
+// )
 
 var (
-	street string
-	city   string
-	state  string
-	zip    string
-	lat    string
-	lng    string
+	address_id int
+	street     string
+	city       string
+	state      string
+	zip        string
+	lat        string
+	lng        string
 )
 
 type arcgisResults struct {
@@ -53,82 +59,98 @@ type arcgisResults struct {
 
 func main() {
 
-	//--------------------------------------------------------------
-	//--- POSTGRES CONNECTION
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+	//--- Get the CREDENTIALS from the env file.
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	host := os.Getenv("DATABASE_HOST")
+	port := os.Getenv("DATABASE_PORT")
+	user := os.Getenv("DATABASE_USERNAME")
+	password := os.Getenv("DATABASE_PASSWORD")
+	dbname := os.Getenv("DATABASE_NAME")
+
+	//--- Make the POSTGRES connection.
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	db, err := sql.Open("postgres", psqlInfo)
+	//log.Println("Made connection to " + host)
 	if err != nil {
+		//log.Println("Connection to " + host + " failed!")
+		//log.Println(err)
 		panic(err)
 	}
 	defer db.Close()
-	//--------------------------------------------------------------
 
-	//--------------------------------------------------------------
-	//--- THIS WILL BRING BACK ROWS THAT NEED TO BE GEOCODED.
-	//--- IN THE FUTURE, LETS LOOK FOR ROWS WITH NULL LAT AND LNG
-	rows, err := db.Query("SELECT street, city, state, zip  FROM address order by address_id desc")
+	//--- Load up a ROWS object with records that need to be geocoded.
+	//--- MT: Should this be a custom function in the database?
+	var sbSql strings.Builder
+	sbSql.WriteString("SELECT ")
+	sbSql.WriteString("		address_id, street, city, state, zip ")
+	sbSql.WriteString("FROM ")
+	sbSql.WriteString("		address ")
+	sbSql.WriteString("WHERE ")
+	sbSql.WriteString("		(lat is null and lng is null) ")
+	sbSql.WriteString("order by address_id desc ")
+
+	strSql := sbSql.String()
+	rows, err := db.Query(strSql)
 	if err != nil {
+		//log.Println("Query failed!")
+		//log.Printin(strSQL)
+		//log.Println(err)
 		panic(err)
 	}
 	defer rows.Close()
-	//--------------------------------------------------------------
 
+	//--- Begin a loop through all ROWS in the row object.
 	for rows.Next() {
-		err := rows.Scan(&street, &city, &state, &zip)
+		err := rows.Scan(&address_id, &street, &city, &state, &zip)
 		if err != nil {
 			panic(err)
 		}
+		//--- Create a full_address string
 		full_address := street + " " + city + " " + state + " " + zip
 
-		//-----------------------------------------------------------------------
-		//--- GEOCODE HERE
-		arcgisUrl := "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?f=pjson&SingleLine=" + (url.QueryEscape(full_address)) + "&outFields=x,y"
+		//--- Now let's craft a proper URL to send to ArcGIS
+		var sbArcGis strings.Builder
+		sbArcGis.WriteString("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/")
+		sbArcGis.WriteString("findAddressCandidates?f=pjson&SingleLine=" + (url.QueryEscape(full_address)))
+		sbArcGis.WriteString("&outFields=x,y")
+		arcgisUrl := sbArcGis.String()
 
+		//--- Fire off the API request.
 		resp, err := http.Get(arcgisUrl)
 
+		//--- Do a little dance here to get the results into the arcgisResults object
 		if err != nil {
 			fmt.Println(err)
 		}
 		defer resp.Body.Close()
 		body, err := io.ReadAll(resp.Body)
-
+		if err != nil {
+			panic(err)
+		}
 		var result arcgisResults
 
+		//--- Error handling, please.
 		if err := json.Unmarshal(body, &result); err != nil { // Parse []byte to the go struct pointer
 			fmt.Println("Can not unmarshal JSON")
 		}
 
-		fmt.Println("---------------------")
-		fmt.Println("Address:" + full_address)
-		fmt.Println("Lng: " + fmt.Sprintf("%f", result.Candidates[0].Location.X))
-		fmt.Println("Lat: " + fmt.Sprintf("%f", result.Candidates[0].Location.Y))
-		fmt.Println("---------------------")
+		//--- Get the lat and lng from the response
+		lat := fmt.Sprintf("%f", result.Candidates[0].Location.Y)
+		lng := fmt.Sprintf("%f", result.Candidates[0].Location.X)
 
-		//var result arcgisResults
+		//--- Build an UPDATE statment for each record that needs it.
+		//--- MT: Also a store proc in the database?
+		var sbSqlUpdate strings.Builder
+		sbSqlUpdate.WriteString("UPDATE address SET ")
+		sbSqlUpdate.WriteString("lat=" + lat)
+		sbSqlUpdate.WriteString(", lng=" + lng)
+		sbSqlUpdate.WriteString(" WHERE address_id = " + strconv.Itoa(address_id))
 
-		// fmt.Println(arcgisUrl)
-		// resp, err := http.Get(arcgisUrl)
-		// fmt.Println(resp.Body)
-
-		// if err != nil {
-		// 	fmt.Println("No response from request")
-		// }
-
-		// //--- Load all the JSON as text into "body"
-		// defer resp.Body.Close()
-
-		// body, err := io.ReadAll(resp.Body) // response body is []byte
-
-		// //--- Unmarshal the JSON into the "result" value
-		// var result arcgisResults
-
-		// //fmt.Println(result)
-		// if err := json.Unmarshal(body, &result); err != nil { // Parse []byte to the go struct pointer
-
-		// 	fmt.Println("Can not unmarshal JSON")
-		// }
-		// //-----------------------------------------------------------------------
+		//--- Run the UPDATEs against the database.
+		fmt.Println(sbSqlUpdate.String())
 
 	}
 	err = rows.Err()
